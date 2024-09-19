25a6a888a7264734ea06c0573c2b7296e2feee42
Aktan <aktanarynov566@gmail.com>
2024-09-19 13:55:05 +0600

    Project Update

--- BarCode/models.py
+++ BarCode/models.py
@@ -6,10 +6,10 @@ class ProductBarCode(models.Model):
     """
     Модель для представления штрих-кода продукта.
     """
-    code = models.CharField(unique=True, max_length=13, verbose_name='Код продукта')
+    code = models.CharField(unique=True, max_length=30, verbose_name='Штрих код продукта')
     product_name = models.CharField(verbose_name='Название продукта')
     bar_type = models.CharField(max_length=6, choices=[('EAN_13', 'EAN_13'), ('UPC-A', 'UPC-A'), ('EAN-8', 'EAN-8'), ('UPC-E', 'UPC-E')], verbose_name='Тип кода продукта')
-    product_status = models.CharField(max_length=42, choices=[('Харам', 'Харам'), ('Халал', 'Халал'), ('Не проверено', 'Не проверено')], verbose_name='Статус продукта Адал/Харам/Не проверено')
+    product_status = models.CharField(max_length=42, choices=[('Проверенный', 'Проверенный'), ('Не проверенный', 'Не проверенный')], verbose_name='Статус продукта Проверенный/Не проверенный')
     company = models.ForeignKey(CertifiedCompany, on_delete=models.CASCADE, verbose_name='Компания')
     adal_center_icon = models.ImageField(upload_to='AdalCenterIcon/', verbose_name='Иконка сертифицированной компании Adal Center')
     product_image = models.ImageField(upload_to='BarCodeProductImages/', verbose_name='Изображение продукта')
